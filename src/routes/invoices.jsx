666a94d6e2e110ea69a56bc4dfe6fada0fb6caec
Ginger Heef <gingerheef003@gmail.com>
2022-09-08 15:55:59 +0530

    reading url parameters

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,5 +1,5 @@
 import { getInvoices } from "../data";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function Invoices() {
     let invoices = getInvoices();
@@ -21,6 +21,7 @@ export default function Invoices() {
                     </Link>
                 ))}
             </nav>
+            <Outlet />
         </div>
     );
 }
